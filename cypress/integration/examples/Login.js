00a8c4ac580edaa24f72fbbdacde171fd27a00c4
Shubham Motwani (c) <shubham.motwani@one10marketing.com>
2023-08-10 18:25:50 +0530

    First Commit

---
+++ cypress/integration/examples/Login.js
@@ -0,0 +1,12 @@
+//<reference types="Cypress" />;
+
+describe("My First Test Suite", function () {
+  it("My FirstTest case", function () {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").type("Admin");
+    cy.get("input[placeholder='Password']").type("admin123");
+    cy.get("button[type='submit']").click();
+  });
+});
