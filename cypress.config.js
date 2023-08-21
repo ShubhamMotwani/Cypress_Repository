cf311dca2cb996781708fc3581ac2f4074932731
Shubham Motwani (c) <shubham.motwani@one10marketing.com>
2023-08-21 18:23:06 +0530

    Added Cypress cloud Configuation

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
-
+//npx cypress run --record --key 05ee75f2-3eae-4532-9936-2cc1c8500df8
 module.exports = defineConfig({
+  projectId: "rpdoj7",
   defaultCommandTimeout: 15000,
   e2e: {
     setupNodeEvents(on, config) {
