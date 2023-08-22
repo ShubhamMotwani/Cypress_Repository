3a0dc03f534c4022caa4da4c4549478f21041f39
Shubham Motwani (c) <shubham.motwani@one10marketing.com>
2023-08-22 16:08:17 +0530

    Added Configuation for  Reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,27 @@
+//cypress.config.js
 const { defineConfig } = require("cypress");
-//npx cypress run --record --key 05ee75f2-3eae-4532-9936-2cc1c8500df8
+
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   projectId: "rpdoj7",
   defaultCommandTimeout: 15000,
+
+  video: false,
+
+  reporterOptions: {
+    charts: true,
+
+    reportPageTitle: "Cypress Inline Reporter",
+
+    embeddedScreenshots: true,
+    reportDir: "cypress/report",
+
+    inlineAssets: true, //Adds the asserts inline
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
     specPattern: "cypress/integration/examples/*.js",
   },
