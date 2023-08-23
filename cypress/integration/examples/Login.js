03580af19ecccd8f98300d919ce6fa4917759628
Shubham12092000 <84227882+Shubham12092000@users.noreply.github.com>
2023-08-23 15:20:26 +0530

    Updated Login.js and changed the script to Navigate to Google

--- cypress/integration/examples/Login.js
+++ cypress/integration/examples/Login.js
@@ -2,11 +2,14 @@
 
 describe("My First Test Suite", function () {
   it("My FirstTest case", function () {
-    cy.visit(
+    // cy.visit(
+    //   "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    // );
+    // cy.get("input[placeholder='Username']").type("Admin");
+    // cy.get("input[placeholder='Password']").type("admin123");
+    // cy.get("button[type='submit']").click();
+   cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
-    cy.get("input[placeholder='Username']").type("Admin");
-    cy.get("input[placeholder='Password']").type("admin123");
-    cy.get("button[type='submit']").click();
+    ); 
   });
 });
