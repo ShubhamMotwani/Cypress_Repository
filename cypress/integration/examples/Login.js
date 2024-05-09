8993a8b064e0b42fd3bbaef955cabdc447302870
ShubhamMotwani <84227882+ShubhamMotwani@users.noreply.github.com>
2024-05-09 19:02:23 +0530

    Update Login.js

--- cypress/integration/examples/Login.js
+++ cypress/integration/examples/Login.js
@@ -2,15 +2,15 @@
 //Hi
 describe("My First Test Suite", function () {
   it("My FirstTest case", function () {
-    // cy.visit(
-    //   "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    // );
-    // cy.get("input[placeholder='Username']").type("Admin");
-    // cy.get("input[placeholder='Password']").type("admin123");
-    // cy.get("button[type='submit']").click();
-   cy.visit(
-      "https://www.google.com/"
-    ); 
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").type("Admin");
+    cy.get("input[placeholder='Password']").type("admin123");
+    cy.get("button[type='submit']").click();
+   // cy.visit(
+   //    "https://www.google.com/"
+   //  ); 
       // expect(3).to.equal(5);
   });
 });
